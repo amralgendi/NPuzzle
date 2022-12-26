53bfcc75120da933900aeab96f65acbd24eda9f2
Mohamed Salah <me@mohasalah.com>
2022-12-26 03:56:30 +0200

    design main menu & remove game settings

--- NPuzzle/MainMenu.cpp
+++ NPuzzle/MainMenu.cpp
@@ -1,28 +1,62 @@
 #include <SFML/Window/Event.hpp>
 #include "MainMenu.hpp"
-#include "GameSettings.hpp"
+#include "ImagePicker.hpp"
 
-MainMenu::MainMenu(std::shared_ptr<Context>& context):m_context(context),isPlayBtnSelected(true ), isExitBtnSelected(false), isPlayBtnPressed(false), isExitBtnPressed(false) {}
+
+MainMenu::MainMenu(std::shared_ptr<Context>& context):m_context(context),isPlayBtnSelected(true ), isExitBtnSelected(false), isPlayBtnPressed(false), isExitBtnPressed(false) {
+	difficulties[EASY] = "3x3";
+	difficulties[MEDIUM] = "4x4";
+	difficulties[HARD] = "5x5";
+
+	puzzleSize[EASY] = 3;
+	puzzleSize[MEDIUM] = 4;
+	puzzleSize[HARD] = 5;
+}
 
 MainMenu::~MainMenu() {}
 
 void MainMenu::init() {
+
 	m_context->m_assets->addFont(MAIN_FONT, "assets\\fonts\\Pacifico-Regular.ttf");
+	m_context->m_assets->addTexture(1021, "assets\\images\\icon.png", true);
+	m_context->m_assets->addTexture(1022, "assets\\images\\left.png", true);
+	m_context->m_assets->addTexture(1023, "assets\\images\\right.png", true);
+
+	m_icon.setTexture(m_context->m_assets->getTexture(1021));
+	m_icon.setPosition(410, 136);
 
 	m_gameTitle.setFont(m_context->m_assets->getFont(MAIN_FONT));
 	m_gameTitle.setString("Puzzle Fuzzle");
-	m_gameTitle.setOrigin(m_gameTitle.getLocalBounds().width / 2, m_gameTitle.getLocalBounds().height / 2);
-	m_gameTitle.setPosition(m_context->m_window->getSize().x / 2.f, m_context->m_window->getSize().y / 6.f);
+	m_gameTitle.setPosition((m_context->m_window->getSize().x / 2)- (m_gameTitle.getLocalBounds().width / 2), 353);
+
+	m_left.setPosition(344, 450);
+	m_left.setTexture(m_context->m_assets->getTexture(1022));
+
+	m_sizeLabel.setFont(m_context->m_assets->getFont(MAIN_FONT));
+	m_sizeLabel.setString("5*5");
+	m_sizeLabel.setPosition((m_context->m_window->getSize().x / 2) - (m_sizeLabel.getLocalBounds().width / 2), m_left.getPosition().y+(m_left.getTexture()->getSize().y/2) - (m_sizeLabel.getLocalBounds().height/2));
+
+	m_right.setPosition(613, 450);
+	m_right.setTexture(m_context->m_assets->getTexture(1023));
+
+	m_playBtnBG.setSize(sf::Vector2f(331, 88));
+	m_playBtnBG.setPosition(344, 563);
+	m_playBtnBG.setFillColor(sf::Color{ 239, 65, 124 });
+	m_playBtnBG.setOutlineThickness(3);
 
 	m_playBtn.setFont(m_context->m_assets->getFont(MAIN_FONT));
 	m_playBtn.setString("Play");
-	m_playBtn.setOrigin(m_playBtn.getLocalBounds().width / 2, m_playBtn.getLocalBounds().height / 2);
-	m_playBtn.setPosition(m_context->m_window->getSize().x / 2.f, m_context->m_window->getSize().y / 2.f);
+	m_playBtn.setPosition(458, 519); 
+	m_playBtn.setPosition(m_playBtnBG.getPosition().x + (m_playBtnBG.getLocalBounds().width / 2) - (m_playBtn.getLocalBounds().width / 2), m_playBtnBG.getPosition().y + (m_playBtnBG.getLocalBounds().height / 2) - (m_playBtn.getLocalBounds().height / 2));
+
+	m_exitBtnnBG.setSize(sf::Vector2f(331, 88));
+	m_exitBtnnBG.setPosition(344, 690);
+	m_exitBtnnBG.setFillColor(sf::Color{ 239, 65, 124 });
+	m_exitBtnnBG.setOutlineThickness(3);
 
 	m_exitBtn.setFont(m_context->m_assets->getFont(MAIN_FONT));
-	m_exitBtn.setString("Exit");
-	m_exitBtn.setOrigin(m_exitBtn.getLocalBounds().width / 2, m_gameTitle.getLocalBounds().height / 2);
-	m_exitBtn.setPosition(m_context->m_window->getSize().x / 2.f, m_context->m_window->getSize().y / 1.5);
+	m_exitBtn.setString("Exit");	
+	m_exitBtn.setPosition(m_exitBtnnBG.getPosition().x + (m_exitBtnnBG.getLocalBounds().width / 2)-(m_exitBtn.getLocalBounds().width / 2), m_exitBtnnBG.getPosition().y +(m_exitBtnnBG.getLocalBounds().height/2)-(m_exitBtn.getLocalBounds().height / 2));
 
 }
 void MainMenu::processInput() {
@@ -55,6 +89,22 @@ void MainMenu::processInput() {
 				else isExitBtnPressed = true;
 				break;
 			}
+
+			case sf::Keyboard::Right: {
+				if (selectedDifficulty < 2) {
+					selectedDifficulty++;
+				}
+				m_sizeLabel.setString(difficulties[selectedDifficulty]);
+				break;
+			}
+			case sf::Keyboard::Left: {
+				if (selectedDifficulty != 0) {
+					selectedDifficulty--;
+				}
+				m_sizeLabel.setString(difficulties[selectedDifficulty]);
+				break;
+			}
+
 			default:
 				break;
 			}
@@ -64,18 +114,27 @@ void MainMenu::processInput() {
 void MainMenu::update(sf::Time) {
 
 	if (isPlayBtnPressed) {
-		m_context->m_states->add(std::make_unique<GameSettings>(m_context));
+		m_context->m_states->add(std::make_unique<ImagePicker>(m_context, puzzleSize[selectedDifficulty]));
 	}
-	m_playBtn.setFillColor(isPlayBtnSelected ? sf::Color::Yellow : sf::Color::White);
-	m_exitBtn.setFillColor(isExitBtnSelected ? sf::Color::Yellow : sf::Color::White);
+	m_playBtnBG.setOutlineColor(isPlayBtnSelected ? sf::Color::White : sf::Color{ 239, 65, 124 });
+	m_exitBtnnBG.setOutlineColor(isExitBtnSelected ? sf::Color::White : sf::Color{ 239, 65, 124 });
 
+	m_sizeLabel.setString(difficulties[selectedDifficulty]);
 
 }
 void MainMenu::draw() {
 
-	m_context->m_window->clear(sf::Color::Blue);
+	m_context->m_window->clear(sf::Color{ 19, 26, 64 });
+	m_context->m_window->draw(m_icon);
+	m_context->m_window->draw(m_left);
+	m_context->m_window->draw(m_sizeLabel);
+	m_context->m_window->draw(m_right);
+
+	m_context->m_window->draw(m_icon);
 	m_context->m_window->draw(m_gameTitle);
+	m_context->m_window->draw(m_playBtnBG);
 	m_context->m_window->draw(m_playBtn);
+	m_context->m_window->draw(m_exitBtnnBG);
 	m_context->m_window->draw(m_exitBtn);
 
 	m_context->m_window->display();
--- NPuzzle/MainMenu.hpp
+++ NPuzzle/MainMenu.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <memory>
-#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics.hpp>
 
 #include "State.hpp"
 #include "Game.hpp"
+
+enum {
+	EASY = 0, MEDIUM = 1, HARD = 2
+};
 
 class MainMenu: public Engine::State {
 public:
@@ -19,14 +23,28 @@ public:
 
 private: 
 	std::shared_ptr<Context> m_context;
+	sf::Sprite m_icon;
 	sf::Text m_gameTitle;
 
+	sf::Sprite m_left;
+	sf::Text m_sizeLabel;
+	sf::Sprite m_right;
+
 	sf::Text m_playBtn;
+	sf::RectangleShape m_playBtnBG;
+
 	sf::Text m_exitBtn;
+	sf::RectangleShape m_exitBtnnBG;
 
 	bool isPlayBtnSelected;
 	bool isPlayBtnPressed;
 
 	bool isExitBtnSelected;
 	bool isExitBtnPressed;
+
+	int selectedDifficulty;
+
+	std::map<int, std::string> difficulties;
+	std::map<int, int> puzzleSize;
+
 };
